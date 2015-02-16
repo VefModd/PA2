4104b00181a0a6918cc6552ac4c255c57ec6d8c5
danben4 <danielbenediktsson4@gmail.com>
2015-02-16 14:19:51 +0000

    Login function - still not completed

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,9 +1,24 @@
 var chatControllers = angular.module('chatControllers', []);
 
-chatControllers.controller('HomeController', ['$scope', '$http',
-  function ($scope, $http) {
-    
-  }]);
+chatControllers.controller('HomeController', ['$scope', '$http', '$location', '$rootScope', '$routeParams',// 'socket',
+	function ($scope, $http, $location, $rootScope, $routeParams, socket) {
+		$scope.errorMsg = '';
+		$scope.nickname = '';
+
+		$scope.login = function() {
+			if($scope.nickname === '') {
+				$scope.errorMsg = 'Please choose a nickname!';
+			} else {
+				socket.emit('adduser', $scope.nickname, function(available) {
+					if(available) {
+						$scope.errorMsg = 'succeed!';
+					} else {
+						$scope.errorMsg = 'This nickname is already in use - please choose another!';
+					}
+				});
+			}
+		};
+	}]);
 
 chatControllers.controller('RoomController', ['$scope', '$routeParams',
 	function ($scope, $routeParams) {
